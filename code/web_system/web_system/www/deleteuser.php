f0e1a06b9c5f06aab1feff78414da3101c5ede82
SivanuHappy <anusha25cse@gmail.com>
2021-12-16 16:55:21 -0500

    Update folder

--- code/web_system/web_system/www/deleteuser.php
+++
@@ -1,7 +0,0 @@
-<?php
-require('config.php');
-$username=$_REQUEST['username'];
-$query = "DELETE FROM users WHERE username='$username'"; 
-$result = mysqli_query($link,$query) or die ( mysqli_error());
-header("Location: admin.php"); 
-?>
--- code/web_system/web_system/www/test.php
+++
@@ -1,5 +0,0 @@
-<!DOCTYPE html>
-<?php
-exec('ABM/jdk/bin/java -cp ABM/plugins/org.eclipse.equinox.launcher*.jar -Xms512m -Xmx2048m -Djava.awt.headless=true org.eclipse.core.launcher.Main -application msi.gama.headless.id4 ABM/headless/samples/roadTraffic.xml test1');
- 
-?>
